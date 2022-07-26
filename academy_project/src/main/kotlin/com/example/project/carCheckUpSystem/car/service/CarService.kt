ddf9987443607d89c17f84eedc5377ff5d972d6d
Marija <marija.andelic02@gmail.com>
2022-07-26 15:41:50 +0200

    Refactor: fetching car models and manufacturers data

--- academy_project/src/main/kotlin/com/example/project/carCheckUpSystem/car/service/CarService.kt
+++ academy_project/src/main/kotlin/com/example/project/carCheckUpSystem/car/service/CarService.kt
@@ -3,20 +3,23 @@ package com.example.project.carCheckUpSystem.car.service
 import com.example.project.carCheckUpSystem.car.controller.dto.AddCarDTO
 import com.example.project.carCheckUpSystem.car.controller.dto.CarDTO
 import com.example.project.carCheckUpSystem.car.controller.dto.CarDetailsDTO
-import com.example.project.carCheckUpSystem.car.entity.CarDetails
 import com.example.project.carCheckUpSystem.car.repository.CarRepository
+import com.example.project.carCheckUpSystem.car.service.exception.CarIdNotFoundException
+import com.example.project.carCheckUpSystem.car.service.exception.CarVinNotFoundException
+import com.example.project.carCheckUpSystem.car.service.exception.VinNotUniqueException
 import com.example.project.carCheckUpSystem.carCheckUp.entity.CarCheckUp
 import com.example.project.carCheckUpSystem.carCheckUp.repository.CarCheckUpRepository
+import com.example.project.carCheckUpSystem.carModel.repository.CarModelRepository
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 import java.util.*
 
-@Service
-class CarService(
+/*@Service
+class CarServiceTHROW(
     private val carRepository: CarRepository,
-    private val carCheckUpRepository: CarCheckUpRepository
+    private val carCheckUpRepository: CarCheckUpRepository,
+    private val carModelRepository: CarModelRepository
 ) {
 
     fun getAllCars() = carRepository.findAll().map { CarDTO(it) }
@@ -44,4 +47,4 @@ class CarService(
     }
 
     fun getAllCars(pageable: Pageable) = carRepository.findAll(pageable).map { CarDTO(it) }
-}
+}*/
